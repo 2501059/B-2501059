f6f7c9d231f19d9be76ec8496229b775541cdfe8
2501059 <kyalkwlww1@kbu.ac.kr>
2025-09-24 17:49:09 +0900

    Delete sample2 directory

--- sample2/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("I'm a new Java programmer.");
-        System.out.println("내 이름은 이웅재 입니다.");
-    }
-}
